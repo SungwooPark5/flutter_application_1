809654f444b5d17cde64f0306d58514f73bee630
Sungwoo Park_computer <sharkclass@naver.com>
2024-05-30 23:18:03 +0900

    studied: Gesture Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,42 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            // 작성하고 싶은 글
-            'Hello World',
-            // 글자에 스타일 적용
-            style: TextStyle(
-              // 글자 크기
-              fontSize: 16.0,
-              // 글자 굵기
-              fontWeight: FontWeight.w700,
-              // 글자 색상
-              color: Colors.red,
+          child: GestureDetector(
+            onTap:(){
+              print('on tap');
+            },
+            onDoubleTap: (){
+              print('on double tap');
+            },
+            onLongPress:(){
+              print('on long press');
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              width: 100.0,
+              height:100.0,
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class FloatingActionButtonExample extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed:(){
+            print('Hi');
+          },
+          child: Text('Hi'),
+        ),
+        body: Container(),
       ),
     );
   }
