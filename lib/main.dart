c053c13db7cc781f6e874fe776bb117b9f373df5
Sungwoo Park_computer <sharkclass@naver.com>
2024-05-30 23:03:02 +0900

    example: 6.3 Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
 void main(){
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
       home: Scaffold(
-        body: Center( 
+        body: Center(
           child: Text(
-          'Hello Code Factory',
+            // 작성하고 싶은 글
+            'Hello World',
+            // 글자에 스타일 적용
+            style: TextStyle(
+              // 글자 크기
+              fontSize: 16.0,
+              // 글자 굵기
+              fontWeight: FontWeight.w700,
+              // 글자 색상
+              color: Colors.red,
+            ),
           ),
-        )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
